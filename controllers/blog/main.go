bb5257dab36f7627c1715e571794f59369dbb002
ruoo <xiongxiangzz@126.com>
2014-12-11 14:59:20 +0800

    增加百度分享,优化标签页内容

--- controllers/blog/main.go
+++ controllers/blog/main.go
@@ -59,12 +59,9 @@ func (this *MainController) Show() {
 	if err != nil || post.Status != 0 {
 		this.Abort("404")
 	}
-
 	post.Views++
 	post.Update("Views")
-
 	post.Content = strings.Replace(post.Content, "_ueditor_page_break_tag_", "", -1)
-
 	this.Data["post"] = post
 	this.setHeadMetas(post.Title, strings.Trim(post.Tags, ","), post.Title)
 	this.display("article")
@@ -135,7 +132,7 @@ func (this *MainController) TagController() {
 		name     string
 		err      error
 		count    int64
-		result   map[string][]*models.Post
+		list     []*models.Post
 	)
 	name = this.Ctx.Input.Param(":name")
 	if page, err = strconv.Atoi(this.Ctx.Input.Param(":page")); err != nil || page < 1 {
@@ -144,49 +141,36 @@ func (this *MainController) TagController() {
 	if pagesize, err = strconv.Atoi(this.getOption("pagesize")); err != nil || pagesize < 1 {
 		pagesize = 20
 	} else {
-		pagesize *= 2
+		pagesize *= 5
 	}
 
 	tagpost := new(models.TagPost)
 	tag := new(models.Tag)
 	tag.Name = name
-
 	if tag.Read("Name") != nil {
 		this.Abort("404")
 	}
+	tagPostQuery := tagpost.Query().Filter("tagid", tag.Id).Filter("poststatus", 0)  //当前标签查询
+	//count, _ = query.Count()
+	tagPostCount,_:=tagPostQuery.Count()
+	if tagPostCount > 0 {
 
-	query := tagpost.Query().Filter("tagid", tag.Id).Filter("poststatus", 0)
-	count, _ = query.Count()
-	result = make(map[string][]*models.Post)
-
-	if count > 0 {
 		var tp []*models.TagPost
-		var list []*models.Post
 		var pids []int64 = make([]int64, 0)
 
-		query.OrderBy("-posttime").Limit(pagesize, (page-1)*pagesize).All(&tp)
+		tagPostQuery.All(&tp)  //标签拥有的文章
 		for _, v := range tp {
 			pids = append(pids, v.Postid)
 		}
-
-		new(models.Post).Query().Filter("id__in", pids).All(&list)
-
-
-		for _, v := range list {
-			year := v.Posttime.Format("2006")
-			if _, ok := result[year]; !ok {
-				result[year] = make([]*models.Post, 0)
-			}
-			result[year] = append(result[year], v)
-		}
+		query := new(models.Post).Query().Filter("id__in", pids)
+		count,_=query.Count()
+		query.OrderBy("-posttime").Limit(pagesize, (page-1)*pagesize).All(&list)
 	}
-
-
 	this.Data["tag"] = tag
 	this.Data["page"] = page
 	this.Data["pagesize"] = pagesize
 	this.Data["count"] = count
-	this.Data["result"] = result
+	this.Data["list"] = list
 	this.Data["pagebar"] = models.NewPager(int64(page), int64(count), int64(pagesize), tag.Link()).ToString()
 
 	this.setHeadMetas(tag.Name, tag.Name, tag.Name)
